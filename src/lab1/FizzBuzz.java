630189a7abf35acfa52bfc97d972d9f99a51e746
Pnoin <kangmatthew0421@gmail.com>
2026-09-15 03:20:51 +0900

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,7 +6,7 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
+        /*
         for (int i = 1; i <= 100; i++) {
 
             // Find out which numbers divide i.
@@ -32,5 +32,50 @@ class FizzBuzz {
 
             }
         }
+        */
+
+
+        // Task 1.1 Rewrite using while loop
+        int i = 1;
+        while (i <= 100) {
+            i = doFizzBuzz(i);
+        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
+
+/*
+* Difference between Java and Python:
+* 1. Semi-colon is required
+* 2. We have to show the type of each variable
+* 3. Public / Private
+* 4. Indentation
+* 5. Brackets for conditions
+* 6. Using curly brackets instead of colons
+* 7. Don't use # for comments
+* */
